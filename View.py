bba924d3b5285b11ca189b3537ec86cab803d5d6
DKudelevich <Delovem@mail.ru>
2022-02-01 16:40:24 +1000

    fixed some things

--- View.py
+++ View.py
@@ -18,27 +18,40 @@ class MainApplicationView(tk.Frame):
         self.label = tk.Label(parent, text="Данные:")
         self.label.grid(row=3)
 
-        self.field = tk.Text(win, height=40, width=72, wrap='word')
+        self.field = tk.Text(parent, height=40, width=72, wrap='word')
         self.field.grid(row=4, column=0, columnspan=3)
 
+        #первый ряд кнопок
+        self.button1 = tk.Button(parent, text='добавить')
+        self.button11 = tk.Button(parent, text='удалить')
+        self.button12 = tk.Button(parent, text='импорт')
+        #размещение первого ряда
+        self.button1.grid(row=0, column=0, stick='we')
+        self.button11.grid(row=0, column=1, stick='we')
+        self.button12.grid(row=0, column=2, stick='we')
 
-        self.button1 = tk.Button(text='добавить').grid(row=0, column=0, stick='we')
-        self.button11 = tk.Button(text='удалить').grid(row=0, column=1, stick='we')
-        self.button12 = tk.Button(text='импорт').grid(row=0, column=2, stick='we')
+        #второй ряд
+        self.button2 = tk.Button(parent, text='Показать список')
+        self.button2.grid(row=1, column=0, columnspan=3,stick='we')
 
-        self.button2 = tk.Button(text='Показать список').grid(row=1, column=0, columnspan=3,stick='we')
+        # третий ряд
+        self.button3 = tk.Button(parent, text='мин').grid(row=2, column=0, stick='we')
+        self.button31 = tk.Button(parent, text='макс').grid(row=2, column=1, stick='we')
+        self.button32 = tk.Button(parent, text='Поиск?').grid(row=2, column=2, stick='we')
 
-        self.button3 = tk.Button(text='мин').grid(row=2, column=0, stick='we')
-        self.button31 = tk.Button(text='макс').grid(row=2, column=1, stick='we')
-        self.button32 = tk.Button(text='Поиск?').grid(row=2, column=2, stick='we')
+        # Четвертный ряд
+        self.button4 = tk.Button(parent, text='О программе').grid(row=5, column=0, stick='we')
+        self.button41 = tk.Button(parent, text='Закрыть').grid(row=5, column=1, columnspan=2, stick='we')
 
-
-        self.button4 = tk.Button(text='О программе').grid(row=5, column=0, stick='we')
-        self.button41 = tk.Button(text='Закрыть').grid(row=5, column=1, columnspan=2, stick='we')
-
-        self.scroller1 = tk.Scrollbar(win, command=self.field.yview)
+        self.scroller1 = tk.Scrollbar(parent, command=self.field.yview)
         self.scroller1.grid(row=2, column=3, rowspan=2, columnspan=2, stick='ns')
 
+        #привязка действия (открыть файл) к кнопке 3 в первом ряду
+        self.button12.bind("<Button-1>", self.open_file)
+
+    def open_file(self, event):
+
+        return askopenfilename()
 
 
 # параметры окна
