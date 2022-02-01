0a5d2e2ade536665a294171aaa432af8d228838f
DKudelevich <Delovem@mail.ru>
2022-02-01 12:46:21 +1000

    created model, view, controller Model.py:  added class Product  added methods for class  added if __name__ == "__main__": View.py:  Nothing Controller.py:  Nothing

---
+++ Model.py
@@ -0,0 +1,33 @@
+class Product():
+    '''Класс определяющий товары'''
+    def __init__(self, name='', price=0, count=0):
+        '''инициализация класса'''
+        self.name = name
+        self.price = price
+        self.count = count
+
+    def product_info(self):
+        '''вывод информации о продукте'''
+        print(f'name = {self.name}, price = {self.price}$, count = {self.count}')
+
+    def add_product(self):
+        '''добавление товара в список'''
+        pass
+    def delete_product(self):
+        '''удаление товара из списка'''
+        pass
+
+    def minprice(self):
+        '''возвращает товар с минимальной ценой'''
+        pass
+
+    def maxprice(self):
+        '''возвращает товар с максимальцой ценой'''
+        pass
+
+
+if __name__ == "__main__":
+    Velociped = Product("Велосипед", 500, 4)
+    Cow = Product("Корова", 3000, 6)
+
+print(Cow.product_info())
